d948bd37cc3f555b4f71940b898c4f8d8ed5e178
Chris Bednarczyk <chris@tourradar.com>
2019-09-04 12:57:47 +0200

    Add alarm on number of requests

--- alarms.tf
+++ alarms.tf
@@ -18,26 +18,6 @@ resource "aws_cloudwatch_metric_alarm" "Invocation4XXErrors" {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "Invocation5XXErrors" {
-  count               = var.invocation_5xx_errors_enabled ? 1 : 0
-  alarm_name          = "${var.alarm_prefix}: Invocation 5xx Errors for ${var.endpoint_name}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = var.invocation_5xx_errors_periods
-  threshold           = var.invocation_5xx_errors_threshold
-  metric_name         = "Invocation5XXErrors"
-  namespace           = "AWS/SageMaker"
-  period              = var.invocation_4xx_errors_period
-  statistic           = "Sum"
-  alarm_description   = "Alarm is above of threshold: ${var.invocation_5xx_errors_threshold}. Looks like endpoint triggering errors."
-  treat_missing_data  = "notBreaching"
-  alarm_actions       = var.actions
-  tags                = var.tags
-
-  dimensions = {
-    EndpointName = var.endpoint_name
-  }
-}
-
 
 resource "aws_cloudwatch_metric_alarm" "Invocation5XXErrors" {
   count               = var.invocation_5xx_errors_enabled ? 1 : 0
